a98125143a10a28412e0bd83a2f1f57cb89382e7
conghoan21 <conghoan.hnc@gmail.com>
2020-09-10 23:43:05 +0700

    add file

--- untitled/src/prime1.java
+++ untitled/src/prime1.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 public class prime1 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập n = ");
+        System.out.print("Enter the integer = ");
         int n = scanner.nextInt();
-        System.out.printf("%d số nguyên tố đầu tiên là: \n", n);
-        int dem = 0; // đếm số số nguyên tố
+        System.out.printf("%d the first integer is: \n", n);
+        int count = 0; // đếm số số nguyên tố
         int i = 2;   // tìm số nguyên tố bắt dầu từ số 2
-        while (dem < n) {
+        while (count < n) {
             if (isPrimeNumber(i)) {
                 System.out.print(i + " ");
-                dem++;
+                count++;
             }
             i++;
         }
     }
 
     private static boolean isPrimeNumber(int n) {
+        // số nguyên n<2 không phải là số nguyên tố
         if (n < 2) {
             return false;
         }
